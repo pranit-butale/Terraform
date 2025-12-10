483fbf38519e719b6ad4bcd53b51c8fa666328c8
pranit-butale <pranitbutale@gmail.com>
2025-12-10 18:18:47 +0530

    Update module.tf

--- module_alb+asg/module.tf
+++ module_alb+asg/module.tf
@@ -1,20 +1,15 @@
 provider "aws" {
-    region = "us-east-1"
-  
+  region = "us-east-1"
 }
 
 module "asg_module" {
 
-    source = "./MAIN-ALB+ASG"
+  source = "./ASG-main"     # keep correct path
 
-    vpc       = var.vpc
-    subnet_1  = var.subnet_1
-    subnet_2  = var.subnet_2
-    lb_type   = var.lb
-    ami_id    = var.image_id
-    instance_type1 = var.instance_type
-
-
-    
-  
+  vpc_cidr        = var.vpc         
+  subnet1         = var.subnet_1     
+  subnet2         = var.subnet_2     
+  lb_type         = var.lb          
+  ami_id1         = var.image_id     
+  instance_type1  = var.instance_type
 }
